6d40d79179a64886f12c55cec7de9e36ffc2d890
Song Ahyeon <song@5works.co.kr>
2023-04-03 10:50:00 +0900

    add layout

--- src/components/MainLayout.tsx
+++ src/components/MainLayout.tsx
@@ -1,18 +1,42 @@
-import { Layout } from "antd";
 import React from "react";
 import { Route } from "react-router";
 import { BrowserRouter as Router, Routes } from "react-router-dom";
+import { Breadcrumb, Layout, Menu, theme } from 'antd';
 import NotFound from "../pages/NotFound";
 import Notice from "../pages/Notice";
 
 const MainLayout = () => {
 
+    const { Header, Content, Footer } = Layout;
+
     const routes = [
         {path: '/notice', element: <Notice />}
     ]
 
-    return (<Layout>
-        <p>isLogin  = true</p>
+    return (
+        <Layout className="layout">
+        <Header>
+          <div className="logo" />
+          <Menu
+            theme="dark"
+            mode="horizontal"
+            defaultSelectedKeys={['2']}
+            items={new Array(15).fill(null).map((_, index) => {
+              const key = index + 1;
+              return {
+                key,
+                label: `nav ${key}`,
+              };
+            })}
+          />
+        </Header>
+        <Content style={{ padding: '0 50px' }}>
+          <Breadcrumb style={{ margin: '16px 0' }}>
+            <Breadcrumb.Item>Home</Breadcrumb.Item>
+            <Breadcrumb.Item>List</Breadcrumb.Item>
+            <Breadcrumb.Item>App</Breadcrumb.Item>
+          </Breadcrumb>
+          
         <Router>
             <Routes>
                 <Route path="/"></Route>
@@ -20,7 +44,14 @@ const MainLayout = () => {
                 <Route path="*" element={<NotFound />}></Route>
             </Routes>
         </Router>
-    </Layout>)
-}
+        
+          <div className="site-layout-content">
+            Content
+          </div>
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>Ant Design ©2023 Created by Ant UED</Footer>
+      </Layout>
+    );
+  };
 
 export default MainLayout
