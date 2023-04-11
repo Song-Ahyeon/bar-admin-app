c6e92299a8fc6bc565e672b247931ee7c7440a07
Song Ahyeon <song@5works.co.kr>
2023-04-11 18:21:36 +0900

    Update Notice.tsx

--- src/pages/Notice.tsx
+++ src/pages/Notice.tsx
@@ -1,10 +1,28 @@
 import { Button, Pagination, Table } from "antd";
+import { ColumnsType } from "antd/es/table";
 import React, { useState } from "react";
+import { NoticeTableDataType } from "../models/notice.dto";
 
 const Notice = () => {
 
+    const initNoticeList = [
+        {title: 'title1', date: '2023-01-01'}
+    ]
+
     // 공지사항 목록
-    const [noticeList, setNoticeList] = useState();
+    const [noticeList, setNoticeList] = useState(initNoticeList);
+    
+    // table columns
+    const columns : ColumnsType<NoticeTableDataType> = [
+        {title: '제목',
+        width: '70%',
+        dataIndex: 'title',
+        align: 'center'},
+        {title: '작성일자',
+        width: '30%',
+        dataIndex: 'date',
+        align: 'center'}
+    ]
 
     return (<>
     <h1>Notice Page</h1> 
@@ -13,7 +31,11 @@ const Notice = () => {
             NEW
         </Button>
     </div>
-    <Table />
+    <Table
+    columns={columns}
+    dataSource={noticeList}
+    pagination={false}
+     />
     <Pagination />
     </>)
 }
