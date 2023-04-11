3ee0da474bd7e0d1c95e45f35a8bda211e7afdfd
Song Ahyeon <song@5works.co.kr>
2023-04-11 18:21:39 +0900

    Update QnA.tsx

--- src/pages/QnA.tsx
+++ src/pages/QnA.tsx
@@ -1,9 +1,12 @@
+import { Pagination, Table } from "antd";
 import React from "react";
 
 const QnA = () => {
 
     return (<>
     <p>QnA page</p>
+    <Table />
+    <Pagination />
     </>)
 }
 
